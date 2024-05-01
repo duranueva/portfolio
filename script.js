7396694a659f58d45f27798ad42d2b84fb89eeac
duranueva <jean.dv@ciencias.unam.mx>
2024-04-30 20:23:33 -0600

    Translating to english (it is not finished). Changing script.js to make the skills section more dynamic. I broke proyects section (Had to fix it).

--- script.js
+++ script.js
@@ -16,26 +16,36 @@ function seleccionar() {
   menuVisible = false;
 }
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades() {
+function efectoHabilidades_columnA() {
   var skills = document.getElementById("skills");
   var distancia_skills =
     window.innerHeight - skills.getBoundingClientRect().top;
   if (distancia_skills >= 300) {
     let habilidades = document.getElementsByClassName("progreso");
-    habilidades[0].classList.add("javascript");
-    habilidades[1].classList.add("htmlcss");
-    habilidades[2].classList.add("photoshop");
-    habilidades[3].classList.add("wordpress");
-    habilidades[4].classList.add("drupal");
-    habilidades[5].classList.add("comunicacion");
-    habilidades[6].classList.add("trabajo");
-    habilidades[7].classList.add("creatividad");
-    habilidades[8].classList.add("dedicacion");
-    habilidades[9].classList.add("proyect");
+    habilidades[0].classList.add("skill-a1");
+    habilidades[1].classList.add("skill-a2");
+    habilidades[2].classList.add("skill-a3");
+    habilidades[3].classList.add("skill-a4");
+    habilidades[4].classList.add("skill-a5");
+  }
+}
+
+function efectoHabilidades_columnB() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[5].classList.add("skill-b1");
+    habilidades[6].classList.add("skill-b2");
+    habilidades[7].classList.add("skill-b3");
+    habilidades[8].classList.add("skill-b4");
+    habilidades[9].classList.add("skill-b5");
   }
 }
 
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function () {
-  efectoHabilidades();
+  efectoHabilidades_columnA();
+  efectoHabilidades_columnB();
 };
